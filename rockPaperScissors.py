452c3bbe50248451084eb40b39f1d4b93ddd64a3
Leonardo Bertoli <leobertoli2091@gmail.com>
2024-01-09 01:48:55 -0300

    Finishing the program

--- rockPaperScissors.py
+++ rockPaperScissors.py
@@ -1,3 +1,5 @@
+import random
+
 rock = '''
     _______
 ---'   ____)
@@ -16,7 +18,7 @@ paper = '''
 ---.__________)
 '''
 
-scissor = '''
+scissors = '''
     _______
 ---'   ____)____
           ______)
@@ -24,5 +26,27 @@ scissor = '''
       (____)
 ---.__(___)
 '''
+print("What do you choose? Type 0 for Rock, 1 for Paper or 2 for Scissors.")
+options = [rock, paper, scissors]
+
+user_input = int(input())
+pc_input = random.randint(0, 2)
 
 
+print(options[user_input])
+print("\nComputer chose:")
+print(options[pc_input])
+
+if pc_input == user_input:
+    print("Draw")
+else:
+    if pc_input != 0:
+        if user_input == pc_input - 1:
+            print("You Lose")
+        else:
+            print("You Win")
+    else:
+        if user_input == 1:
+            print("You Win")
+        else:
+            print("You Lose")
